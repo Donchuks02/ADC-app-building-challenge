0a5d018159ba04d6de99db813a7a65e923ec2774
Donchuks02 <chukwudidavid02@gmail.com>
2023-11-08 22:45:27 +0100

    added some info features to the pop-up container

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const inputFieldValue = document.querySelector('#inputField')
 const submitButton = document.querySelector('#SubmitBtn')
 const popUpEl = document.querySelector('.pop-up-section')
 const errorMessage = document.querySelector('#errorMsg')
+const total_purchase = document.querySelector('#totalPurchase')
+const roundedUpValue = document.querySelector('#roundedUpValue')
+const amountDonated = document.querySelector('#donatedAmount')
 const yesButton = document.querySelector('#yesBtn')
 const noButton = document.querySelector('#noBtn')
+
+
+
+
+
+
 
 //  Event listener function for the Submit button
 submitButton.addEventListener('click', function() {
@@ -12,7 +21,20 @@ submitButton.addEventListener('click', function() {
     if (inputValue == false) {
         errorMessage.textContent = 'Please enter the total purchase'
 
-    } else(popUpEl.classList.remove('hidden'))
+    } else(popUpEl.classList.remove('hidden')); {
+
+
+        total_purchase.textContent = inputValue
+        roundedUpValue.textContent = `${Math.ceil(inputValue)}.00`;
+
+        let rndValue = Math.ceil(inputValue) - inputValue
+        let roundedResult = parseFloat(rndValue.toPrecision(12))
+        amountDonated.textContent = roundedResult
+
+    }
+
+
+
 
 
 
@@ -27,15 +49,18 @@ yesButton.addEventListener('click', function() {
     inputValue = result
     popUpEl.classList.add('hidden')
     inputFieldValue.value = `${inputValue}.00`;
-    errorMessage.textContent = ` `
+    errorMessage.textContent = ' '
     submitButton.classList.add('hidden')
 
 })
+
+
 
 //  Event listener function for the No button
 
 noButton.addEventListener('click', function() {
     popUpEl.classList.add('hidden')
     submitButton.classList.add('hidden')
+    errorMessage.textContent = ' '
 
 })
